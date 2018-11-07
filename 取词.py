43e337a0aa9c25bf993754a0668423c34c9b635b
fangjingyi <692363386@qq.com>
2018-11-07 21:32:57 +0800

    Create 取词.py

---
+++ 取词.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+# -*- coding: cp936 -*-
+import jieba
+import jieba.posseg as pseg
+import codecs
+import re
+import os
+import time
+import string
+from nltk.probability import FreqDist
